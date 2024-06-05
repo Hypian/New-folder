8561152429c138b86bde5c61d52a06a3121f0b89
Bryan <kagingobrian2002@gmail.com>
2024-06-05 21:08:31 +0300

    Pilot

---
+++ Python Shennigans.py
@@ -0,0 +1,17 @@
+#Pilot
+print("Hello World") #code execution
+#declaration
+age = 20
+print(age)
+#string declaration
+sentence = "my name is hypian"
+print(sentence)
+#multiple declaration
+sarah,bob,mike = 12,1,"tree"
+print(sarah,bob,mike)
+#basically if the values arent the same number as the respective variables then it will
+#result into an error (value error with an unexpected value number) 
+sarah = bob = mike = 223
+car,colour,brand = "toyota","red","nissan"
+print(car,colour,brand)
+#Arithmetic Operations
